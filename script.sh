c344c18ba63d5f252d3169f7a95b3b8b227c240b
starship-boi <91227993+starship-boi@users.noreply.github.com>
2022-12-10 16:24:33 +0000

    function definitions - WIP

--- script.sh
+++ script.sh
@@ -1,8 +1,38 @@
 function check_internet_connection {
-    curl https://github.com/starship-boi/fedora-setup/test
+    if [ $(curl https://github.com/starship-boi/fedora-setup/test.txt) == "Yay, it works!" ]
+    then
+        echo "You are connected to the Internet."
+    else
+        echo "You are not connected to the Internet: aborting..."
+        
+        exit 3
+    fi
+}
+
+function check_sudo {
+    if [ $(id -u) != "0" ]
+    then
+        echo "This script must be run using root permissions (for example with 'sudo'): aborting..."
+    fi
+}
+
+function edit_dnf_config {
+    sudo echo -e "fastestmirror=True \nmax_parallel_downloads=10 \ndeltarpm=-2 \ndeltarpm_percentage=99" >> /etc/dnf/dnf.conf
+}
+
+function remove_packages {
+    sudo dnf remove -y evince firefox
+}
+
+function upgrade_dnf_packages {
+    sudo dnf update -y --refresh
+    
+    
 }
 
 check_internet_connection
+
+check_sudo
 
 edit_dnf_config
 
