75eedc5e7482f46ceaf9af121984d77566e9c17c
starship-boi <91227993+starship-boi@users.noreply.github.com>
2023-06-10 19:40:38 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -44,7 +44,7 @@ function install_rpms {
     sudo dnf install -y gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
     sudo dnf install -y lame* --exclude=lame-devel
     sudo dnf group upgrade -y --with-optional --allowerasing --skip-broken Multimedia
-    sudo dnf install -y ffmpeg ffmpeg-libs libva libva-utils intel-media-driver mozilla-openh264 gnome-shell-extension-pop-shell xprop distrobox gparted @virtualization tlp tlp-rdw
+    sudo dnf install -y ffmpeg ffmpeg-libs libva libva-utils intel-media-driver mozilla-openh264 gnome-shell-extension-pop-shell xprop distrobox gparted @virtualization tlp tlp-rdw gnome-tweaks
     
 }
 
