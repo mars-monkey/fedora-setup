2e544734f8f22b585ed55219a08a8baeff8b7f33
starship-boi <91227993+starship-boi@users.noreply.github.com>
2023-06-13 19:15:31 +0100

    Update script.sh
    after testing

--- script.sh
+++ script.sh
@@ -1,65 +1,58 @@
-GREEN="\033[0;32m"
-NC="\033[0m"
+#!/bin/sh
 
-function run_as_root {
-    sudo echo
+run_as_root() {
+    sudo printf ''
 }
 
-function edit_dnf_config {
-    echo -e "${GREEN}Configuring DNF..."
+edit_dnf_config() {
+    printf '\nConfiguring DNF...\n\n'
     
-    echo "gpgcheck=True" | sudo tee /etc/dnf/dnf.conf
-    echo "skip_if_unavailable=True" | sudo tee -a /etc/dnf/dnf.conf
-    echo "deltarpm=False" | sudo tee -a /etc/dnf/dnf.conf
-    echo "fastest_mirror=True" | sudo tee -a /etc/dnf/dnf.conf
-    echo "max_parallel_downloads=20" | sudo tee -a /etc/dnf/dnf.conf
-    echo "minrate=32k" | sudo tee -a /etc/dnf/dnf.conf
-    echo "timeout=20" | sudo tee -a /etc/dnf/dnf.conf
+    printf 'deltarpm=False\nmax_parallel_downloads=10\ndefaultyes=True\n' | sudo tee -a /etc/dnf/dnf.conf
 }
 
-function remove_rpms {
-    echo -e "${GREEN}Removing RPM packages..."
+remove_rpms() {
+    printf '\nRemoving RPM packages...\n\n'
     
     sudo dnf remove -y gnome-boxes gnome-calculator gnome-calendar gnome-characters cheese gnome-clocks gnome-connections gnome-contacts gnome-disk-utility simple-scan evince mediawriter firefox gnome-font-viewer gnome-color-manager eog libreoffice* gnome-logs gnome-maps gnome-photos rhythmbox gnome-terminal text-editor gnome-tour totem gnome-weather
 }
 
-function upgrade_rpms {
-    echo -e "${GREEN}Upgrading RPM packages..."
+upgrade_rpms() {
+    printf '\nUpgrading RPM packages...\n\n'
     
-    sudo dnf update -y --refresh
+    sudo dnf upgrade -y --refresh
 }
 
-function install_rpmfusion {
-    echo -e "${GREEN}Installing RPM Fusion repositories..."
+install_rpmfusion() {
+    printf '\nInstalling RPM Fusion repositories...\n\n'
     
-    sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-    sudo dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+    sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-'$(rpm %fedora)'.noarch.rpm
+    sudo dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-'$(rpm %fedora)'.noarch.rpm
     
     sudo dnf groupupdate -y core
 }
 
-function install_rpms {
-    echo -e "${GREEN}Installing RPM packages..."
+install_rpms() {
+    printf '\nInstalling RPM packages...\n\n'
     
-    sudo dnf install -y gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
+    sudo dnf install -y gstreamer1-plugins-bad-\* gstreamer1-plugins-good-\* gstreamer1-plugins-base gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
     sudo dnf install -y lame* --exclude=lame-devel
     sudo dnf group upgrade -y --with-optional --allowerasing --skip-broken Multimedia
     sudo dnf install -y ffmpeg ffmpeg-libs libva libva-utils intel-media-driver mozilla-openh264 gnome-shell-extension-pop-shell xprop distrobox gparted @virtualization tlp tlp-rdw gnome-tweaks
     
 }
 
-function install_flatpaks {
-    echo -e "${GREEN}Installing flatpaks..."
+install_flatpaks() {
+    printf '\nInstalling flatpaks...\n\n'
     
     flatpak install # 
 }
 
-function set_gsettings {
-    echo -e "${GREEN}Configuring GNOME..."
+set_gsettings() {
+    printf '\nConfiguring GNOME...\n\n'
 }
 
-function sync_dotfiles {
-    echo -e "${GREEN}Syncing app configurations..."
+sync_dotfiles() {
+    printf '\nSyncing app configurations...\n\n'
 }
 
 run_as_root
@@ -70,12 +63,12 @@ remove_rpms
 
 upgrade_rpms
 
-install_rpmfusion
+#install_rpmfusion
 
-install_rpms
+#install_rpms
 
-install_flatpaks
+#install_flatpaks
 
-set_gsettings
+#set_gsettings
 
-sync_dotfiles
+#sync_dotfiles
